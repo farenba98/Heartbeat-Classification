085afefdf5ba41734566cc19658ee3ed6cacf338
Faren <farnoushbaghestani@gmail.com>
2023-04-23 19:40:26 -0400

    segmented

--- main.py
+++ main.py
@@ -13,4 +13,18 @@ ann_samples = ann.sample
 ann_symbols = ann.symbol
 
 # wfdb.plot_items(signal=signal, ann_samp=[ann_samples], ann_sym=[ann_symbols])
-# plt.show()
+# plt.savefig('sig.png')
+
+segmented_beats = []
+seg_len = 300
+
+for i, peak in enumerate(ann_samples):
+    beat_type = ann_symbols[i]
+    if peak > 150 and beat_type in ["N", "S", "V", "F", "Q"]:
+        segment = signal[int(peak-seg_len/2):int(peak+seg_len/2)]
+        # Append segment and corresponding type to segmented_beats
+        segmented_beats.append((segment, beat_type))
+
+first_segment, first_label = segmented_beats[0]
+print("Signal segment:", first_segment)
+print("Label:", first_label)
