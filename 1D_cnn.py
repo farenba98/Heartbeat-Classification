4d2af311ad4ac613b5835caed8a160c3af089738
Faren <farnoushbaghestani@gmail.com>
2023-04-25 15:57:40 -0400

    network created

--- 1D_cnn.py
+++ 1D_cnn.py
@@ -1,15 +1,24 @@
 from keras.models import Sequential
-from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense
+from keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, Dropout, Softmax
 
-# # Define the model
-# model = Sequential()
-# model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(input_shape)))
-# model.add(MaxPooling1D(pool_size=2))
-# model.add(Flatten())
-# model.add(Dense(units=10, activation='softmax'))
 
-# # Compile the model
-# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model = Sequential()
+model.add(Conv1D(filters = 64, kernel_size = 5, activation = 'relu', input_shape = (300, 1)))
+model.add(Conv1D(filters = 64, kernel_size = 5, activation = 'relu'))
+model.add(MaxPooling1D(pool_size = 2))
+model.add(Dropout(rate = 0.5))
+model.add(Conv1D(filters = 128, kernel_size = 3, activation = 'relu'))
+model.add(Conv1D(filters = 128, kernel_size = 3, activation = 'relu'))
+model.add(MaxPooling1D(pool_size = 2))
+model.add(Dropout(rate = 0.5))
+model.add(Flatten())
+model.add(Dense(units = 256, activation = 'relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(units = 128, activation = 'relu'))
+model.add(Dropout(rate=0.5))
+model.add(Softmax())
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# # Train the model
-# model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
+# Train the model
+model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
